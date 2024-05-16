4aa024f373edc6ed59673bbc8eb57f9de6eef84a
NIhat <135535115+Nihatgunay@users.noreply.github.com>
2024-05-16 12:12:29 +0400

    added project

---
+++ tek ededler.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK---1
+
+            //int a = 6; 
+            //int b = 87;
+            //int count = 0; 
+
+            //for (int i = a; i < b; i++)
+            //{ 
+            //    if (i % 2 ==1)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+
+            
+
+
+        }
+    }
+}
